e44949fbdfda7e5df2b5e4f62638355f8fd4650d
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2020-12-16 19:47:03 +0300

    added spring.jpa.generate-ddl=true to application.properties. Now post method is working.

--- src/main/java/com/kavgorodov/course/service/impl/UserServiceImpl.java
+++ src/main/java/com/kavgorodov/course/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.kavgorodov.course.service.impl;
 
+import com.kavgorodov.course.UserRepository;
+import com.kavgorodov.course.io.entity.UserEntity;
 import com.kavgorodov.course.service.UserService;
 import com.kavgorodov.course.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,8 +14,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("testPassword");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
