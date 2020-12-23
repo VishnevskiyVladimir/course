86acb276bba284001b697891e79359811b4bd0b3
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2020-12-23 13:17:05 +0300

    added AddressRequestModel class, and AddressDTO class. Added field addresses to UserDetailsRequestModel

--- src/main/java/com/kavgorodov/course/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/com/kavgorodov/course/ui/model/request/UserDetailsRequestModel.java
@@ -1,5 +1,7 @@
 package com.kavgorodov.course.ui.model.request;
 
+import java.util.List;
+
 /**
  * @author Vladimir Vishnevskiy
  */
@@ -9,37 +11,36 @@ public class UserDetailsRequestModel {
     private String lastName;
     private String email;
     private String password;
-
+    private List<AddressRequestModel> addresses;
 
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+    public List<AddressRequestModel> getAddresses() {
+        return addresses;
+    }
+    public void setAddresses(List<AddressRequestModel> addresses) {
+        this.addresses = addresses;
     }
 }
